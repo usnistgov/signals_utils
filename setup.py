2f040e1c74a175305b227ee14be05689b842bf04
Xavier SCHMITT <xavier.schmitt@nist.gov>
2019-05-20 14:48:30 -0400

    chore: upgrade 1.1.0.dev1
    Change-Id: I7c922b9a5745372a56168e399828e406e1fc978d

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="signals_utils",
-    version="1.0.0",
+    version="1.1.0.dev1",
     description="Mongo signals utils for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
